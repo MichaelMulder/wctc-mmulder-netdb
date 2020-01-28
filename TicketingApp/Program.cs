04114ecd95f6a8e7cfdcaf6b1819561dfa11d7d4
BIT student <bitstudent@bitnet.wctc.edu>
2020-01-27 20:38:32 -0600

    Add project files.

---
+++ TicketingApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = 0;
+            string fileName = "Tickets.csv";
+            while (choice != 3)
+            {
+                Console.WriteLine("1) Read Data from file");
+                Console.WriteLine("2) Create file from data");
+                Console.WriteLine("3) Exit");
+
+                string input = Console.ReadLine();
+
+                choice = int.Parse(input);
+
+
+                switch (choice)
+                {
+                    case 1:
+                        if (File.Exists(fileName))
+                        {
+                            StreamReader sr = new StreamReader(fileName); 
+                            while(!sr.EndOfStream)
+                            {
+
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("File does not exist");
+                        }
+                        break;
+                    case 2:
+                        break; 
+                    case 3:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
