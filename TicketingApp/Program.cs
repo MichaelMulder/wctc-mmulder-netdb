e829fcf4ef8e22ce9c2d46cb83f657375be785c8
BIT student <bitstudent@bitnet.wctc.edu>
2020-01-28 14:32:34 -0600

    Reads from csv file and appends data to the existing csv file

--- TicketingApp/Program.cs
+++ TicketingApp/Program.cs
@@ -23,29 +23,68 @@ namespace TicketingApp
 
                 choice = int.Parse(input);
 
-
                 switch (choice)
                 {
                     case 1:
                         if (File.Exists(fileName))
                         {
-                            StreamReader sr = new StreamReader(fileName); 
+                            StreamReader sr = new StreamReader(fileName);
+                            // Read the first line and set it as the header for the data points
+                            string[] headers = sr.ReadLine().Split(','); 
                             while(!sr.EndOfStream)
                             {
-                                string line = sr.ReadLine();
+                                string line = sr.ReadLine(); 
 
-                                string[] arr = line.Split(',');
+                                string[] data = line.Split(',');
 
-                                Console.WriteLine($"TicketID: {arr[0]}");
 
+                                int i = 0;
+                                foreach(var header in headers)
+                                {
+                                    Console.WriteLine($"{header}: {data[i]}");
+                                    i++; 
+                                } 
                             }
+                            sr.Close();
                         }
                         else
                         {
                             Console.WriteLine("File does not exist");
+                            Console.WriteLine(Directory.GetCurrentDirectory());
                         }
                         break;
                     case 2:
+                        StreamWriter sw = new StreamWriter(fileName, true); 
+
+                        Console.WriteLine("Enter TicketID:");
+ 
+                        string ticketID = Console.ReadLine();
+
+                        Console.WriteLine("Write summary for ticket:");
+
+                        string summary = Console.ReadLine();
+
+                        Console.WriteLine("Enter status of ticket:");
+
+                        string status = Console.ReadLine();
+
+                        Console.WriteLine("Enter proirity of status:");
+
+                        string priority = Console.ReadLine();
+
+                        Console.WriteLine("Who is summiting this ticket:");
+
+                        string submitter = Console.ReadLine();
+
+                        Console.WriteLine("Who is assigned for this ticket:");
+
+                        string assigned = Console.ReadLine();
+
+                        Console.WriteLine("Who is watching this ticket:");
+                        string watchers = Console.ReadLine();
+
+                        sw.WriteLine($"\n{ticketID},{summary}, {status}, {priority}, {submitter}, {assigned}, {watchers}"); 
+                        sw.Close();
                         break; 
                     case 3:
                         break;
