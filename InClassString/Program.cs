60446174a3b364d26d1e483652865e20fe0396fa
Michael Mulder <mmulder4@my.wctc.edu>
2020-02-03 18:59:39 -0600

    Added all projects needed for class from NETDBProgramming

---
+++ InClassString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jimmy";
+            string address = "123 Pine St";
+            Console.WriteLine("|{0, 4}| lives on |{1, 40}| Okay?", name, address);
+
+            var pi = Math.PI;
+
+            Console.WriteLine($"{pi}");
+
+        }
+    }
+}
