289b6c97a29884b0024bdbe444f049a9dda19492
MichaelMulder <mmulder4@my.wctc.edu>
2020-02-04 15:07:44 -0600

    changed brace style, changed collections to generics

--- SleepData/Program.cs
+++ SleepData/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.IO;
-using System.Collections;
+using System.Collections.Generic;
 
-namespace SleepData
-{
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
+namespace SleepData {
+    class MainClass {
+        public static void Main(string[] args) {
             // ask for input
             Console.WriteLine("Enter 1 to create data file.");
             Console.WriteLine("Enter 2 to parse data.");
@@ -19,8 +16,7 @@ namespace SleepData
             // string file = "~/Downloads/data.txt";
             string file = AppDomain.CurrentDomain.BaseDirectory + "data.txt";
 
-            if (resp == "1")
-            {
+            if (resp == "1") {
                 // create data file
                 
                 // ask a question
@@ -41,12 +37,10 @@ namespace SleepData
                 // create file
                 StreamWriter sw = new StreamWriter(file);
                 // loop for the desired # of weeks
-                while (dataDate < dataEndDate)
-                {
+                while (dataDate < dataEndDate) {
                     // 7 days in a week
                     int[] hours = new int[7];
-                    for (int i = 0; i < hours.Length; i++)
-                    {
+                    for (int i = 0; i < hours.Length; i++) {
                         // generate random number of hours slept between 4-12 (inclusive)
                         hours[i] = rnd.Next(4, 13);
                     }
@@ -58,23 +52,20 @@ namespace SleepData
                 }
                 sw.Close();
             }
-            else if (resp == "2")
-            {
-                // TODO: parse data file into hash table
+            else if (resp == "2") {
                 /*
                  * Table structure
                  * WeekKey              HoursSleptValue   
                  * -------              ----------
-                 * DateTime             int[7]
+                 * DateTime             List<int>
                  * 1/1/2020             8|9|6|10|12|8|7
                  */
 
-                Hashtable sleepData = new Hashtable();
+                Dictionary<DateTime, List<int>> sleepData = new Dictionary<DateTime, List<int>>();
 
                 StreamReader sr = new StreamReader(file);
 
-                while(!sr.EndOfStream)
-                {
+                while(!sr.EndOfStream) {
                     string[] fileData = sr.ReadLine().Split(',');
 
                    
@@ -82,15 +73,18 @@ namespace SleepData
 
                     string[] weekhoursData = fileData[1].Split('\n');
 
-                    ArrayList weekDates = convertWeeksToWeekDates(weeksData);
+                    List<DateTime> weekDates = convertWeeksToWeekDates(weeksData);
                     Console.WriteLine(weekDates[0]);
                     int i = 0;
-                    foreach(var week in weekDates)
-                    {
+                    foreach(var week in weekDates) {
                         var dataHours = weekhoursData[i];
                         var hours = weekHoursDataToArray(dataHours);
                         sleepData.Add(week, hours);
                         i++;
+                    }
+
+                    foreach(DictionaryEntry de in sleepData) {
+                        
                     }
 
                 }
@@ -98,11 +92,11 @@ namespace SleepData
             }
 
             /**
-             * Convert a string array of weeks into an arraylist of DateTimes
+             * Convert a string array of weeks into an list of DateTimes
              */
  
-            ArrayList convertWeeksToWeekDates(string[] weeks) { 
-                var weekDates = new ArrayList();
+            List<DateTime> convertWeeksToWeekDates(string[] weeks) { 
+                var weekDates = new List<DateTime>();
                 foreach(var week in weeks) { 
                     weekDates.Add(DateTime.Parse(week));
                 } 
@@ -112,12 +106,11 @@ namespace SleepData
 
             /**
              * Turns a string of numbers seperated like 6|8|9
-             * into an arraylist of numbers 
+             * into an list of numbers 
              */
 
-            ArrayList weekHoursDataToArray(string weekHours)
-            {
-                var hours = new ArrayList();
+            List<int> weekHoursDataToArray(string weekHours) {
+                var hours = new List<int>();
                 string[] dataHours;
                 dataHours = weekHours.Split('|');
                 foreach(var hour in dataHours) {
