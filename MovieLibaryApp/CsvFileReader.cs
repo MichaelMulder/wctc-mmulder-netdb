590e0a4e6e23dcaefe44fd9f99b5325a57689c32
MichaelMulder <mmulder4@my.wctc.edu>
2020-02-06 19:35:06 -0600

    Movied classes to a singlefile for each class. Made fileReader and Writer. started data to list converters

---
+++ MovieLibaryApp/CsvFileReader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic; 
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibaryApp {
+    class CSVFileReader : IfileReader { 
+        private bool headersAvaliable;
+        private string[] headers;
+        private string[] data;
+
+        public CSVFileReader(bool headersAvaiable) {
+            this.headersAvaliable = headersAvaiable; 
+        }
+
+        public string[] Headers { get => headers; } 
+        public string[] Data { get => data; }
+
+        public void readFromFile(string fileName) {
+            try {
+                if (File.Exists(fileName)) {
+                    StreamReader sr = new StreamReader(fileName + ".csv");
+                    // Read the first line and set it as the header for the data points
+                    if(this.headersAvaliable) { 
+                        this.headers = sr.ReadLine().Split(',');
+                    }
+                    while (!sr.EndOfStream) {
+                        string line = sr.ReadLine();
+
+                        this.data = line.Split(',');
+                    }
+                    sr.Close();
+                }
+            } catch(Exception e) {
+                Console.WriteLine(e); 
+            }
+        }
+
+
+    }
+}
---
+++ MovieLibaryApp/IfileWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibaryApp {
+    interface IfileWriter {
+
+        void writeToFile(string text, string fileName);
+    }
+}
