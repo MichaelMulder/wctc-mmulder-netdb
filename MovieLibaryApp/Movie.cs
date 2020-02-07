c6b9c42f5706491d9f9fc356fea4ca5bbe0c07a5
Michael Mulder <mmulder4@my.wctc.edu>
2020-02-06 19:35:06 -0600

    Movied classes to a singlefile for each class. Made fileReader and Writer. started data to list converters

---
+++ MovieLibaryApp/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibaryApp {
+    class Movie {
+        private int id;
+        private string title;
+        private int year;
+        private List<MovieGenres> genres;
+
+        public int Year { get => year; set => year = value; }
+
+        public string Title { get => title; set => title = value; } 
+
+        internal List<MovieGenres> Genres { get => genres; set => genres = value; }
+    }
+}
