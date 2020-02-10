48de221aaff1faf9e2a1fad470ef5183aa81d3d7
BIT student <bitstudent@bitnet.wctc.edu>
2020-02-10 15:56:22 -0600

    Ported over weekend work from laptob with core, added search functions
     Committer: Michael Mulder <mmulder4@my.wctc.edu>

--- MovieLibaryApp/Program.cs
+++ MovieLibaryApp/Program.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.IO;
+﻿using System; 
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace MovieLibaryApp {
-    class Program {
+namespace MovieLibaryApp
+{
+ class Program {
         static void Main(string[] args) {
             int choice = 0;
-            string fileName = "Movies";
+            string fileName = "Movies.csv";
             while (choice != 3) {
                 Console.WriteLine("1) Search for a movie");
                 Console.WriteLine("2) Create a Movie for the database");
@@ -20,13 +18,17 @@ namespace MovieLibaryApp {
 
                 choice = int.Parse(input);
 
-                List<Movie> movieList;
 
                 switch (choice) {
                     case 1:
                         fr.readFromFile(fileName);
-                        string[] headers = fr.Headers;
-                        string[] data = fr.Data;
+                        string[] headers = fr.Headers; 
+                        var data = fr.Data;
+
+                        var myMoives = dataToMovieList(data);
+
+                        displayResults(myMoives);
+
                         break;
                     case 2:
                         break;
@@ -37,32 +39,75 @@ namespace MovieLibaryApp {
                 }
             } 
         }
-        Movie dataLineToMovie(string dataLine) {
-            return null;
+
+        static Movie dataLineToMovie(string[] dataLine) { 
+            var movie = new Movie();
+            for(int i = 0; i < dataLine.Length; i++) { 
+                movie.ID = int.Parse(dataLine[0]);
+                movie.Title = dataLine[1];
+                movie.Genres = parseStringToMovieGenres(dataLine[2]);
+            }
+            return movie;
         }
 
-        List<Movie> dataToMovieList(string[] data) {
-            return null;
+        static List<Movie> dataToMovieList(List<string[]> data) {
+            var movieList = new List<Movie>();
+            foreach (var dataLine in data) {
+                movieList.Add(dataLineToMovie(dataLine));
+            }
+
+            return movieList;
         } 
+
+        static List<MovieGenres> parseStringToMovieGenres(string dataGenres) {
+            var genres = new List<MovieGenres>();
+            string[] stringGenres = dataGenres.Split('|');
+            foreach(var stringGenre in stringGenres) { 
+                if(stringGenre == "Film-Noir") {
+                    genres.Add(MovieGenres.FilmNoir);
+                }
+
+                if (stringGenre == "(no genres listed)") {
+                    genres.Add(MovieGenres.NoGenre);
+                }
+
+                if(stringGenre == "Sci-Fi") {
+                    genres.Add(MovieGenres.SciFi);
+                }
+                    
+                if(Enum.TryParse(stringGenre, out MovieGenres genre)) { 
+                    genres.Add(genre); 
+                } 
+            }
+            return genres;
+        }
         
-        string movieToDataLine(Movie movie) { 
+        static string[] movieToDataLine(Movie movie) {
+            string[] newDataLine = { movie.ID, movie.Title, };
+            
             return null;
         } 
 
-        List<Movie> searchByName(string searchTerm) {
-            // TODO
-            return null;
+        static List<Movie> searchByName(string searchTerm, List<Movie> movieList) {
+            var results = movieList.FindAll(m => m.Title.StartsWith(searchTerm));
+            return results;
+        } 
+
+        static List<Movie> searchByGenre(MovieGenres genre, List<Movie> movieList) {
+            var results = movieList.FindAll(m => m.Genres.Contains(genre));
+            return results;
         }
 
-        List<Movie> searchByYear(string year) {
-            // TODO
-            return null;
-        }
+        static void displayResults(List<Movie> movies) { 
+            foreach(var movie in movies) {
+                var movieGenres = String.Join(", ", movie.Genres.ToArray());
+                System.Console.WriteLine($"| {"ID", -6}| {"Title", -20} | {"Genres", 3} "); 
+                System.Console.WriteLine($"|-------|----------------------|-------");
+                System.Console.WriteLine($"|{movie.ID, 7}|{movie.Title, -22}| {movieGenres}");
+                System.Console.WriteLine("\n");
+            }
 
-        List<Movie> searchByGenre(params MovieGenres[] genres) {
-            // TODO
-            return null;
         }
     }
-}
 
+}
