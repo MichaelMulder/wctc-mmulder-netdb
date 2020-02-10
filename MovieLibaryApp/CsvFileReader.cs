f9c3e05c8d694a9176de3259d1dc85b2821f1c2b
BIT student <bitstudent@bitnet.wctc.edu>
2020-02-10 15:56:22 -0600

    Ported over weekend work from laptob with core, added search functions
     Committer: Michael Mulder <mmulder4@my.wctc.edu>

--- MovieLibaryApp/CsvFileReader.cs
+++ MovieLibaryApp/CsvFileReader.cs
@@ -4,32 +4,44 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
 
 namespace MovieLibaryApp {
     class CSVFileReader : IfileReader { 
         private bool headersAvaliable;
         private string[] headers;
-        private string[] data;
+        private List<string[]> data;
 
         public CSVFileReader(bool headersAvaiable) {
             this.headersAvaliable = headersAvaiable; 
         }
 
         public string[] Headers { get => headers; } 
-        public string[] Data { get => data; }
+        public List<string[]> Data { get => data; }
 
         public void readFromFile(string fileName) {
             try {
                 if (File.Exists(fileName)) {
-                    StreamReader sr = new StreamReader(fileName + ".csv");
+                    StreamReader sr = new StreamReader(fileName);
                     // Read the first line and set it as the header for the data points
                     if(this.headersAvaliable) { 
                         this.headers = sr.ReadLine().Split(',');
                     }
+
+                    List<string[]> data = new List<string[]>();
                     while (!sr.EndOfStream) {
                         string line = sr.ReadLine();
 
-                        this.data = line.Split(',');
+                        Regex regx = new Regex(',' + "(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+
+                        string[] dataLine = regx.Split(line);
+
+
+                        data.Add(dataLine);
+
+
+                        this.data = data;
                     }
                     sr.Close();
                 }
