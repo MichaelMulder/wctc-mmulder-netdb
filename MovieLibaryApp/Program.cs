e0c38a016486f3d2558e59a3a7817691c71b53e3
Michael Mulder <mmulder4@my.wctc.edu>
2020-02-10 17:53:02 -0600

    Added menus for searching and adding movies

--- MovieLibaryApp/Program.cs
+++ MovieLibaryApp/Program.cs
@@ -7,32 +7,92 @@ namespace MovieLibaryApp
  class Program {
         static void Main(string[] args) {
             int choice = 0;
-            string fileName = "Movies.csv";
+            string fileName = "movies.csv";
             while (choice != 3) {
-                Console.WriteLine("1) Search for a movie");
-                Console.WriteLine("2) Create a Movie for the database");
-                Console.WriteLine("3) Exit");
 
+                Console.WriteLine("1) Search movies");
+                Console.WriteLine("2) Add movie");
+                Console.WriteLine("3) Exit"); 
+ 
+
+                
                 string input = Console.ReadLine();
                 CSVFileReader fr = new CSVFileReader(true);
 
                 choice = int.Parse(input);
 
+                fr.readFromFile(fileName);
+                string[] headers = fr.Headers; 
+                var data = fr.Data; 
 
+
+                var movieList = dataToMovieList(data);
                 switch (choice) {
                     case 1:
-                        fr.readFromFile(fileName);
-                        string[] headers = fr.Headers; 
-                        var data = fr.Data;
+                        Console.WriteLine("1) Search by title");
+                        Console.WriteLine("2) Search by genre");
+                        Console.WriteLine("3) Display All"); 
+ 
+                        int searchChoice;
+                        string searchInput = Console.ReadLine();
+                        searchChoice = int.Parse(searchInput);
+                        switch (searchChoice) {
+                            
+                            case 1:
+                                Console.WriteLine("Enter a Movie title");
+                                var searchTitle = Console.ReadLine();
+                                displayResults(searchByName(searchTitle, movieList));
+                                break; 
+                            case 2:
+                                Console.WriteLine("Enter a movie genre");
+                                var searchGenre = Console.ReadLine(); 
+                                if (Enum.TryParse(searchGenre, out MovieGenres genre)) { 
+                                    displayResults(searchByGenre(genre, movieList));
+                                } else {
+                                    Console.WriteLine("Please enter one of the following genres");
+                                    Console.WriteLine(" Action, " + "Adventure, " + "Animation, " +
+                                        "Children, " +
+                                        "Comedy, " +
+                                        "Crime, " +
+                                        "Documentary, " +
+                                        "Drama, " +
+                                        "Fantasy, " +
+                                        "FilmNoir, " +
+                                        "Horror, " +
+                                        "Musical, " +
+                                        "Mystery, " +
+                                        "Romance, " +
+                                        "SciFi, " +
+                                        "Thriller, " +
+                                        "War, " +
+                                        "Western, NoGenre"); 
+                                } 
+                                break;
+                            
+                            case 3: 
+                                displayResults(movieList);
+                                break;
 
-                        var myMoives = dataToMovieList(data);
+                            default:
+                                break;
 
-                        displayResults(myMoives);
+                        } 
 
                         break;
-                    case 2:
+                    case 2: 
+                        var newMovie = new Movie();
+                        newMovie.ID = movieList.Count + 1;
+                        Console.WriteLine("Enter the title of the Movie");
+                        string movieTitle = Console.ReadLine();
+                        newMovie.Title = movieTitle; 
+                        Console.WriteLine("Enter the genres of the Movie seperated by |");
+                        string movieGenres = Console.ReadLine();
+                        newMovie.Genres = parseStringToMovieGenres(movieGenres); 
+                        movieList.Add(newMovie);
+                        
                         break;
                     case 3:
+                        Environment.Exit(0);
                         break;
                     default:
                         break;
@@ -77,15 +137,15 @@ namespace MovieLibaryApp
                     
                 if(Enum.TryParse(stringGenre, out MovieGenres genre)) { 
                     genres.Add(genre); 
-                } 
+                }
             }
             return genres;
         }
         
         static string[] movieToDataLine(Movie movie) {
-            string[] newDataLine = { movie.ID, movie.Title, };
-            
-            return null;
+            var genres = String.Join("|", movie.Genres.ToArray());
+            string[] newDataLine = { movie.ID.ToString(), movie.Title, genres }; 
+            return newDataLine;
         } 
 
         static List<Movie> searchByName(string searchTerm, List<Movie> movieList) {
@@ -94,7 +154,7 @@ namespace MovieLibaryApp
         } 
 
         static List<Movie> searchByGenre(MovieGenres genre, List<Movie> movieList) {
-            var results = movieList.FindAll(m => m.Genres.Contains(genre));
+            var results = movieList.FindAll(m => m.Genres.Contains(genre)); 
             return results;
         }
 
