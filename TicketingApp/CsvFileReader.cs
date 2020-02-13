dd0c6b9ab190b61d88af039cbcf49862c646132e
MichaelMulder <mmulder4@my.wctc.edu>
2020-02-13 11:09:17 -0600

    Ported over CSV file readers, wrote the data structure for Tickets.

---
+++ TicketingApp/CsvFileReader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic; 
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace MovieLibaryApp {
+    class CSVFileReader : IfileReader { 
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        private bool headersAvaliable;
+        private string[] headers;
+        private List<string[]> data;
+
+        public CSVFileReader(bool headersAvaiable) {
+            this.headersAvaliable = headersAvaiable; 
+        }
+
+        public string[] Headers { get => headers; } 
+        public List<string[]> Data { get => data; }
+
+        public void readFromFile(string fileName) {
+            try {
+                if (File.Exists(fileName)) {
+                    StreamReader sr = new StreamReader(fileName);
+                    // Read the first line and set it as the header for the data points
+                    if(this.headersAvaliable) { 
+                        this.headers = sr.ReadLine().Split(',');
+                    }
+
+                    List<string[]> data = new List<string[]>();
+                    while (!sr.EndOfStream) {
+                        string line = sr.ReadLine();
+
+                        Regex regx = new Regex(',' + "(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+
+                        string[] dataLine = regx.Split(line);
+
+
+                        data.Add(dataLine);
+
+
+                        this.data = data;
+                    }
+                    sr.Close();
+                }
+            } catch(Exception e) {
+
+                logger.Error(e.Message); 
+            }
+        }
+
+
+    }
+}
