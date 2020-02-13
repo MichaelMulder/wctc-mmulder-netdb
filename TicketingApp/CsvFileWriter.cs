82b2295ddde3ae2c228719856937dfe6fa6fd54a
MichaelMulder <mmulder4@my.wctc.edu>
2020-02-13 11:09:17 -0600

    Ported over CSV file readers, wrote the data structure for Tickets.

---
+++ TicketingApp/CsvFileWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibaryApp {
+    class CSVFileWriter : IfileWriter { 
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public void writeToFile(string[] dataLine, string fileName) {
+            try {
+                StreamWriter sw  = new StreamWriter(fileName, true);
+                string newLine = string.Join(",", dataLine);
+                sw.WriteLine(newLine);
+                sw.Close();
+            } catch (Exception e) {
+                logger.Error(e.Message);
+                Console.WriteLine(e);
+            }
+        }
+    }
+
+}
---
+++ TicketingApp/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketingApp {
+    class Ticket {
+        public int TicketID { get; set; }
+        public string Summary { get; set; } 
+        public TicketStatus TicketStatus { get; set; }
+        public Priority Priority { get; set; }
+        public string Submitter { get; set; }
+        public string Assgined { get; set; }
+        public string[] Watching { get; set; }
+
+    }
+}
