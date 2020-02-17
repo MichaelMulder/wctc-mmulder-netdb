50595b709a6eff40e54e728ad4c4c1fb6d576d84
MichaelMulder <mmulder4@my.wctc.edu>
2020-02-17 16:55:33 -0600

    Fixed logic error for checking the title is already in the DB. Renamed the function to avoid this

--- MovieLibaryApp/MovieManager.cs
+++ MovieLibaryApp/MovieManager.cs
@@ -66,7 +66,7 @@ namespace MovieLibaryApp {
             return  this.movieList.FindAll(m => m.Genres.Contains(genre)); 
         }
 
-        public bool TitleIsVaild(string title) { 
+        public bool TitleExists(string title) { 
             return this.movieList.Exists(m => m.Title.Contains(title.ToLower())); 
         }
     }
