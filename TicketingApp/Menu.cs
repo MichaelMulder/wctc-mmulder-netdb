90660c877cb1c8ce48b4ba92b24efb1d28db843c
MichaelMulder <mmulder4@my.wctc.edu>
2020-02-24 21:05:12 -0600

    Refacoring menu and program

--- TicketingApp/Menu.cs
+++ TicketingApp/Menu.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TicketingApp.TicketTypes;
+using TicketingApp.States;
 
 namespace TicketingApp {
     class Menu {
@@ -38,6 +40,151 @@ namespace TicketingApp {
                 Console.WriteLine("Please enter vaild input (a number)");
             }
             return parsedInput;
+        }
+
+        public static BugTicket GetBugTicket(TicketState state) {
+
+            var listLength = state.TicketList.Count;
+
+            Console.WriteLine("Write summary for ticket:");
+
+            string summary = Console.ReadLine();
+
+            Console.WriteLine("Enter status of ticket(Open, Closed, Pending, Resovled):");
+
+            string status = Console.ReadLine();
+
+            Console.WriteLine("Enter priority of status(High, Medium, Low):");
+
+            string sPriority = Console.ReadLine();
+
+            Console.WriteLine("Who is summiting this ticket:");
+
+            string submitter = Console.ReadLine();
+
+            Console.WriteLine("Who is assigned for this ticket:");
+
+            string assgined = Console.ReadLine();
+
+            Console.WriteLine("Who is watching this ticket(seperated by |):");
+            string watchers = Console.ReadLine();
+
+            Console.WriteLine("What is the severity of the bug(Minor, Major, Critical):");
+            string sSeverity = Console.ReadLine();
+
+            var newTicket = new BugTicket {
+                TicketID = state.TicketList[listLength - 1].TicketID + 1,
+                Summary = summary.ToLower(),
+                TicketStatus = Enum.TryParse(status, out TicketStatus ticketStatus) ? ticketStatus : TicketStatus.Error, // ticketStatus parser
+                Priority = Enum.TryParse(sPriority, out Priority priority) ? priority : Priority.Error, // ticketPriority parser
+                Submitter = submitter.ToLower(),
+                Assgined = assgined.ToLower(),
+                Watching = watchers.ToLower().Split('|').ToList(),
+                Severity = Enum.TryParse(sSeverity, out Severity severity) ? severity : Severity.Error
+            };
+            return newTicket;
+        }
+        public static EnhancementTicket GetEnhancementTicket(TicketState state) {
+
+            var listLength = state.TicketList.Count;
+
+            Console.WriteLine("Write summary for ticket:");
+
+            string summary = Console.ReadLine();
+
+            Console.WriteLine("Enter status of ticket(Open, Closed, Pending, Resovled):");
+
+            string status = Console.ReadLine();
+
+
+            Console.WriteLine("Enter priority of status(High, Medium, Low):");
+
+            string sPriority = Console.ReadLine();
+
+            Console.WriteLine("Who is summiting this ticket:");
+
+            string submitter = Console.ReadLine();
+
+            Console.WriteLine("Who is assigned for this ticket:");
+
+            string assgined = Console.ReadLine();
+
+            Console.WriteLine("Who is watching this ticket(seperated by |):");
+            string watchers = Console.ReadLine();
+
+            Console.WriteLine("What is the software:");
+            string software = Console.ReadLine();
+
+            Console.WriteLine("What is the cost (ex. 1.00)");
+            string sCost = Console.ReadLine();
+
+            Console.WriteLine("What is the reason");
+            string reason = Console.ReadLine();
+
+            Console.WriteLine("What is the estimate");
+            string estimate = Console.ReadLine();
+
+            var newETicket = new EnhancementTicket {
+                TicketID = state.TicketList[listLength - 1].TicketID + 1,
+                Summary = summary.ToLower(),
+                TicketStatus = Enum.TryParse(status, out TicketStatus ticketStatus) ? ticketStatus : TicketStatus.Error, // ticketStatus parser
+                Priority = Enum.TryParse(sPriority, out Priority priority) ? priority : Priority.Error, // ticketPriority parser
+                Submitter = submitter.ToLower(),
+                Assgined = assgined.ToLower(),
+                Watching = watchers.ToLower().Split('|').ToList(),
+                Software = software.ToLower(),
+                Cost = decimal.TryParse(sCost, out decimal cost) ? cost : 0,
+                Reason = reason.ToLower(),
+                Estimate = estimate.ToLower()
+            };
+            return newETicket;
+        }
+
+        public static TaskTicket GetTaskTicket(TicketState state) {
+            var listLength = state.TicketList.Count;
+
+            Console.WriteLine("Write summary for ticket:");
+
+            string summary = Console.ReadLine();
+
+            Console.WriteLine("Enter status of ticket(Open, Closed, Pending, Resovled):");
+
+            string status = Console.ReadLine();
+
+
+            Console.WriteLine("Enter priority of status(High, Medium, Low):");
+
+            string sPriority = Console.ReadLine();
+
+            Console.WriteLine("Who is summiting this ticket:");
+
+            string submitter = Console.ReadLine();
+
+            Console.WriteLine("Who is assigned for this ticket:");
+
+            string assgined = Console.ReadLine();
+
+            Console.WriteLine("Who is watching this ticket(seperated by |):");
+            string watchers = Console.ReadLine();
+
+            Console.WriteLine("What is the Task Name:");
+            string taskName = Console.ReadLine();
+
+            Console.WriteLine("What is the Due Date:");
+            string dueDate = Console.ReadLine();
+
+            var newTaskTicket = new TaskTicket {
+                TicketID = state.TicketList[listLength - 1].TicketID + 1,
+                Summary = summary.ToLower(),
+                TicketStatus = Enum.TryParse(status, out TicketStatus ticketStatus) ? ticketStatus : TicketStatus.Error, // ticketStatus parser
+                Priority = Enum.TryParse(sPriority, out Priority priority) ? priority : Priority.Error, // ticketPriority parser
+                Submitter = submitter.ToLower(),
+                Assgined = assgined.ToLower(),
+                Watching = watchers.ToLower().Split('|').ToList(),
+                TaskName = taskName.ToLower(),
+                DueDate = dueDate.ToLower()
+            };
+            return newTaskTicket;
         }
         public static void displayResults(List<Ticket> ticketList) {
             int pageSize = 3, pageCounter = 0; 
